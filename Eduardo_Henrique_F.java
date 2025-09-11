8e8fe8fb3c0790590f87c370bd489d34a45cec5a
Eduardotzz <edduardohenrique76@gmail.com>
2025-09-11 20:41:17 -0300

    Eduardo Henrique Fernandes Ferreira

---
+++ Eduardo_Henrique_F.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// atividade01_Eduardo_henrique_Fernandes.java
+class Main {
+    public static void main(String[] args) {
+   
+        int a; 
+        int b; 
+        int c; 
+        int d; 
+
+        a = 25;
+        b = 25*3;
+        c = 10;
+        d = a/c;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
