b3bd9fb218af6d33e3dcfdf5c5b944048e4967bf
Eduardo.Hemrique <edduardohenrique76@gmail.com>
2025-10-15 20:46:29 -0300

    Update classAluno.java

--- Atvd008qst09.java/classAluno.java
+++ Atvd008qst09.java/classAluno.java
@@ -5,7 +5,7 @@ public class Gabarito {
 
     public Gabarito() {
         respostas = new char[15];
-        // exemplo de gabarito (pode ser alterado)
+     
         char[] exemplo = {'A','B','C','D','E','A','B','C','D','E','A','B','C','D','E'};
         for (int i = 0; i < 15; i++) {
             respostas[i] = exemplo[i];
