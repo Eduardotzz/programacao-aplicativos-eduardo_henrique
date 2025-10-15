7c79c873c59ff739dfad0e8b6b17db0b5c810b2a
Eduardo.Hemrique <edduardohenrique76@gmail.com>
2025-10-15 20:40:00 -0300

    Rename package from 'desafio.dois' to 'AtividadeAluno'

---
+++ Atvd008qst09.java/classAluno.java
@@ -0,0 +1,18 @@
+package AtividadeAluno;
+
+public class Gabarito {
+    private char[] respostas;
+
+    public Gabarito() {
+        respostas = new char[15];
+        // exemplo de gabarito (pode ser alterado)
+        char[] exemplo = {'A','B','C','D','E','A','B','C','D','E','A','B','C','D','E'};
+        for (int i = 0; i < 15; i++) {
+            respostas[i] = exemplo[i];
+        }
+    }
+
+    public char respostaQuestao(int numeroQuestao) {
+        return respostas[numeroQuestao];
+    }
+}
